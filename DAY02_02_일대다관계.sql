d8ca4f1e782eab7d44c4968cdeaf4af2c9cb26e3
wnals9 <wnals16@naver.com>
2023-06-30 14:51:10 +0900

    230630

---
+++ DAY02_02_일대다관계.sql
@@ -0,0 +1,33 @@
+/*
+    1:M 관계
+    1. 2개의 테이블을 관계 짓는 가장 대표적인 관계
+    2. 1    : M
+       PK   : FK
+       부모  : 자식
+    3. 반드시 부모 테이블을 먼저 만들고, 자식 테이블은 나중에 만들어야 한다.
+    4. 반드시 자식 테이블을 먼저 지우고, 부모 테이블은 나중에 지워야 한다.
+*/
+
+-- 자식 먼저 지우기
+DROP TABLE STUDENT_T;
+
+--부모 나중에 지우기
+DROP TABLE SCHOOL_T;
+
+
+
+-- 부모 먼저 만들기
+CREATE TABLE SCHOOL_T (
+    SCH_CODE NUMBER            NOT NULL
+   ,SCH_NAME VARCHAR2(10 BYTE) NOT NULL
+   ,CONSTRAINT PK_SCH PRIMARY KEY(SCH_CODE) -- 제약조건의 이름은 PK_SCH, SCH_CODE에 PRIMARY KEY를 지정
+);
+
+-- 자식 나중에 만들기
+CREATE TABLE STUDENT_T (
+    STU_NO   NUMBER            NOT NULL
+   ,SCH_CODE NUMBER            
+   ,STU_NAME VARCHAR2(10 BYTE) NOT NULL
+   ,CONSTRAINT PK_STU PRIMARY KEY(STU_NO)  -- 제약조건의 이름은 PK_STU, STU_NO에 PRIMARY KEY를 지정
+   ,CONSTRAINT FK_SCH_STU FOREIGN KEY(SCH_CODE) REFERENCES SCHOOL_T(SCH_CODE) --제약조건의 이름은 FK_SCH_STU, SCH_CODE는 SCHOOL_T 테이블의 SCH_CODE를 참조한다.
+);
---
+++ DAY02_03_다대다관계.sql
@@ -0,0 +1,38 @@
+/*
+    M:N 관계
+    1. 현실세계에서 빈번히 나타나지만 주의해야 하는 관계
+    2. M:N 관계를 가진 2개의 테이블은 직접 관계를 맺는 것이 불가능하다.
+    3. 관계를 맺기 위해서 별도의 테이블이 추가로 필요하다.
+    4. M:N 관계는 1:M 관계 2개로 구현할 수 있다.
+*/
+
+-- 삭제는 생성의 역순으로 진행
+DROP TABLE ENROLL_T;
+DROP TABLE SUBJECT_T;
+DROP TABLE STUDENT_T;
+
+
+-- 학생 테이블
+CREATE TABLE STUDENT_T (
+    STU_NO   NUMBER            NOT NULL PRIMARY KEY
+   ,STU_NAME VARCHAR2(10 BYTE) NOT NULL
+   ,AGE      NUMBER
+);
+
+-- 과목 테이블
+CREATE TABLE SUBJECT_T (
+    SBJ_CODE  VARCHAR2(5 BYTE)  NOT NULL PRIMARY KEY
+   ,SBJ_NAME  VARCHAR2(10 BYTE) NOT NULL
+   ,PROFESSOR VARCHAR2(10 BYTE) NOT NULL
+);
+
+-- 수강신청 테이블
+CREATE TABLE ENROLL_T (
+    EN_NO    NUMBER           NOT NULL PRIMARY KEY
+   ,STU_NO   NUMBER           REFERENCES STUDENT_T(STU_NO)
+   ,SBJ_CODE VARCHAR2(5 BYTE) REFERENCES SUBJECT_T(SBJ_CODE)
+);
+
+
+--학생 테이블<-> 과목 테이블은 서로 연관이없으므로 임의 순서로 만들어도됨
+
