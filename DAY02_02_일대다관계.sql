801eae5a483eaa72c2868bbee9267c3b66f0bb5b
wnals9 <wnals16@naver.com>
2023-06-30 16:14:17 +0900

    230630

--- DAY02_02_일대다관계.sql
+++ DAY02_02_일대다관계.sql
@@ -8,6 +8,12 @@
     4. 반드시 자식 테이블을 먼저 지우고, 부모 테이블은 나중에 지워야 한다.
 */
 
+/*
+    삭제 옵션
+    1. ON DELETE CASCADE  : 외래키가 참조하는 기본키 값이 삭제되면 외래키도 함께 삭제한다.
+    2. ON DELETE SET NULL : 외래키가 참조하는 기본키 값이 삭제되면 외래키를 NULL로 처리한다.
+    
+*/
 -- 자식 먼저 지우기
 DROP TABLE STUDENT_T;
 
@@ -29,5 +35,5 @@ CREATE TABLE STUDENT_T (
    ,SCH_CODE NUMBER            
    ,STU_NAME VARCHAR2(10 BYTE) NOT NULL
    ,CONSTRAINT PK_STU PRIMARY KEY(STU_NO)  -- 제약조건의 이름은 PK_STU, STU_NO에 PRIMARY KEY를 지정
-   ,CONSTRAINT FK_SCH_STU FOREIGN KEY(SCH_CODE) REFERENCES SCHOOL_T(SCH_CODE) --제약조건의 이름은 FK_SCH_STU, SCH_CODE는 SCHOOL_T 테이블의 SCH_CODE를 참조한다.
+   ,CONSTRAINT FK_SCH_STU FOREIGN KEY(SCH_CODE) REFERENCES SCHOOL_T(SCH_CODE) ON DELETE CASCADE --제약조건의 이름은 FK_SCH_STU, SCH_CODE는 SCHOOL_T 테이블의 SCH_CODE를 참조한다.
 );
